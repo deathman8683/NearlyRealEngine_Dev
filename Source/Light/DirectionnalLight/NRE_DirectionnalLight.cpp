fd406c58952d4485f7b40ebf551d476a1057c215
ABEL Louis <abel.louis.88@gmail.com>
2018-04-06 10:09:43 +0200

    Fix directionnal light

--- Source/Light/DirectionnalLight/NRE_DirectionnalLight.cpp
+++ Source/Light/DirectionnalLight/NRE_DirectionnalLight.cpp
@@ -7,7 +7,7 @@
             DirectionnalLight::DirectionnalLight() {
             }
 
-            DirectionnalLight::DirectionnalLight(Maths::Point3D<NREfloat> const& coord, Maths::Vector3D<NREfloat> const& color, Maths::Maths::Vector3D<NREfloat> direction, NREfloat const& ambientCoeff) :
+            DirectionnalLight::DirectionnalLight(Maths::Point3D<NREfloat> const& coord, Maths::Vector3D<NREfloat> const& color, Maths::Vector3D<NREfloat> direction, NREfloat const& ambientCoeff) :
                 Light(Maths::Point4D<NREfloat>(coord, 0.0), color, direction, 0.0, ambientCoeff, 360.0) {
             }
 
