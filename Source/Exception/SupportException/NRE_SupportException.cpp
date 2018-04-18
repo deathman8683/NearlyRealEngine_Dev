1863098f4d8d96a55235ea9c15f3a011f4bb5e22
ABEL Louis <abel.louis.88@gmail.com>
2018-04-18 16:21:12 +0200

    Documentation Phase + Move Phase : SupportException

--- Source/Exception/SupportException/NRE_SupportException.cpp
+++ Source/Exception/SupportException/NRE_SupportException.cpp
@@ -13,7 +13,20 @@
             SupportException::SupportException(SupportException const& e) throw() : ExceptionHandler::ExceptionHandler(e) {
             }
 
+            SupportException::SupportException(SupportException && e) throw() : ExceptionHandler::ExceptionHandler(std::move(e)) {
+            }
+
             SupportException::~SupportException() throw() {
+            }
+
+            SupportException& SupportException::operator=(SupportException const& e) {
+                ExceptionHandler::operator=(e);
+                return *this;
+            }
+
+            SupportException& SupportException::operator=(SupportException && e) {
+                ExceptionHandler::operator=(std::move(e));
+                return *this;
             }
 
         };
