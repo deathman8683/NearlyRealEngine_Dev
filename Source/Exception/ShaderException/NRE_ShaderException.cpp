391095f985489e34b102da2ef257e5734916be57
ABEL Louis <abel.louis.88@gmail.com>
2018-04-18 16:19:49 +0200

    Documentation Phase + Move Phase : ShaderException

--- Source/Exception/ShaderException/NRE_ShaderException.cpp
+++ Source/Exception/ShaderException/NRE_ShaderException.cpp
@@ -13,7 +13,20 @@
             ShaderException::ShaderException(ShaderException const& e) throw() : ExceptionHandler::ExceptionHandler(e) {
             }
 
+            ShaderException::ShaderException(ShaderException && e) throw() : ExceptionHandler::ExceptionHandler(std::move(e)) {
+            }
+
             ShaderException::~ShaderException() throw() {
+            }
+
+            ShaderException& ShaderException::operator=(ShaderException const& e) {
+                ExceptionHandler::operator=(e);
+                return *this;
+            }
+
+            ShaderException& ShaderException::operator=(ShaderException && e) {
+                ExceptionHandler::operator=(std::move(e));
+                return *this;
             }
 
         };
--- Source/Exception/ShaderException/NRE_ShaderException.hpp
+++ Source/Exception/ShaderException/NRE_ShaderException.hpp
@@ -28,14 +28,49 @@
             class ShaderException : public ExceptionHandler {
                 public:
                     //## Constructor ##//
-                    ShaderException() throw();
-                    ShaderException(std::string const& log) throw();
+                        /**
+                         * Default Constructor
+                         */
+                        ShaderException() throw();
+                        /**
+                         * Construct an exception with a custom log
+                         * @param log the exception's log
+                         */
+                        ShaderException(std::string const& log) throw();
 
                     //## Copy-Constructor ##//
-                    ShaderException(ShaderException const& e) throw();
+                        /**
+                         * Copy e into this
+                         * @param e the exception to copy the content
+                         */
+                        ShaderException(ShaderException const& e) throw();
+
+                    //## Move-Constructor ##//
+                        /**
+                         * Move e into this, leaving e empty
+                         * @param e the exception to move
+                         */
+                        ShaderException(ShaderException && e) throw();
 
                     //## Deconstructor ##//
-                    virtual ~ShaderException() throw();
+                        /**
+                         * ShaderException Deconstructor
+                         */
+                        virtual ~ShaderException() throw();
+
+                    //## Assignment Operator ##//
+                        /**
+                         * Copy assigment of e into this
+                         * @param e the exception to copy into this
+                         * @return the reference of himself
+                         */
+                        ShaderException& operator=(ShaderException const& e);
+                        /**
+                         * Move assigment of e into this, leaving e empty
+                         * @param e the exception to move into this
+                         * @return the reference of himself
+                         */
+                        ShaderException& operator=(ShaderException && e);
             };
 
         };
