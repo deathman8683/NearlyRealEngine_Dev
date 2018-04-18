ba77f50d204402b4bfab8ce2ddfb19a881c211c1
ABEL Louis <abel.louis.88@gmail.com>
2018-04-18 16:29:23 +0200

    Documentation Phase : ArrayOperation

--- Source/Generic/Array/NRE_ArrayOperation.hpp
+++ Source/Generic/Array/NRE_ArrayOperation.hpp
@@ -22,11 +22,53 @@
          */
         namespace Array {
 
+            /**
+             * Compute the index of a 4D cell in a 1D array
+             * @param  p    the point with 4D index
+             * @param  size the 4D array size
+             * @return      the computed index
+             */
             GLuint const get1DIndexFrom4D(Maths::Point4D<GLuint> const& p, Maths::Vector4D<GLuint> const& size);
+            /**
+             * Compute the index of a 4D cell in a 1D array
+             * @param  x    the first dimension index
+             * @param  y    the second dimension index
+             * @param  z    the third dimension index
+             * @param  w    the fourth dimension index
+             * @param  size the 4D array size
+             * @return      the computed index
+             */
             GLuint const get1DIndexFrom4D(GLuint const& x, GLuint const& y, GLuint const& z, GLuint const& w, Maths::Vector4D<GLuint> const& size);
+            /**
+             * Compute the index of a 3D cell in a 1D array
+             * @param  p    the point with 3D index
+             * @param  size the 3D array size
+             * @return      the computed index
+             */
             GLuint const get1DIndexFrom3D(Maths::Point3D<GLuint> const& p, Maths::Vector3D<GLuint> const& size);
+            /**
+             * Compute the index of a 3D cell in a 1D array
+             * @param  x    the first dimension index
+             * @param  y    the second dimension index
+             * @param  z    the third dimension index
+             * @param  size the 3D array size
+             * @return      the computed index
+             */
             GLuint const get1DIndexFrom3D(GLuint const& x, GLuint const& y, GLuint const& z, Maths::Vector3D<GLuint> const& size);
+            /**
+             * Compute the index of a 2D cell in a 1D array
+             * @param  p    the point with 2D index
+             * @param  size the 2D array size
+             * @return      the computed index
+             */
             GLuint const get1DIndexFrom2D(Maths::Point2D<GLuint> const& p, Maths::Vector2D<GLuint> const& size);
+            /**
+             * Compute the index of a 2D cell in a 1D array
+             * @param  x    the first dimension index
+             * @param  y    the second dimension index
+             * @param  size the 2D array size
+             * @return      the computed index
+             */
             GLuint const get1DIndexFrom2D(GLuint const& x, GLuint const& y, Maths::Vector2D<GLuint> const& size);
 
         };
