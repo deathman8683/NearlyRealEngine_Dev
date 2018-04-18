b5573c3edd16f9e6ffad3fff4ba1c29f463fd247
ABEL Louis <abel.louis.88@gmail.com>
2018-04-18 18:22:48 +0200

    Documentation Phase + Move Phase : VAO

--- Source/GL_Wrapper/Buffer/VAO/NRE_VAO.cpp
+++ Source/GL_Wrapper/Buffer/VAO/NRE_VAO.cpp
@@ -13,7 +13,7 @@
                 }
             }
 
-            VAO::VAO(VAO const& buf) : Buffer::Buffer(buf) {
+            VAO::VAO(VAO && buf) : Buffer::Buffer(std::move(buf)) {
             }
 
             VAO::~VAO() {
@@ -40,6 +40,11 @@
                 bind();
                     buffer.access(vertexType, enableVAA);
                 unbind();
+            }
+
+            VAO& VAO::operator=(VAO && buf) {
+                Buffer::operator=(std::move(buf));
+                return *this;
             }
 
         };
--- Source/GL_Wrapper/Buffer/VAO/NRE_VAO.hpp
+++ Source/GL_Wrapper/Buffer/VAO/NRE_VAO.hpp
@@ -31,31 +31,81 @@
 
                 public:
                     //## Constructor ##//
-                    VAO();
-                    VAO(bool const& generate);
+                        /**
+                         * Default Constructor
+                         */
+                        VAO();
+                        /**
+                         * Construct the vao and generate or not his id
+                         * @param generate tell if the base class has to generate an id or not
+                         */
+                        VAO(bool const& generate);
 
                     //## Copy-Constructor ##//
-                    VAO(VAO const& buf);
+                        /**
+                         * No copy allowed
+                         * @param buf the vao to copy
+                         */
+                        VAO(VAO const& buf) = delete;
+
+                    //## Move-Constructor ##//
+                        /**
+                         * Move buf into this, leaving buf empty
+                         * @param buf the vao to move
+                         */
+                        VAO(VAO && buf);
 
                     //## Convertor ##//
 
                     //## Deconstructor ##//
-                    virtual ~VAO();
+                        /**
+                         * VAO Deconstructor
+                         */
+                        ~VAO();
 
                     //## Getter ##//
 
                     //## Setter ##//
 
                     //## Methods ##//
-                    void generateID() override;
-                    void deleteID() override;
-                    void bind() const override;
-                    void unbind() const override;
-                    void access(VBO const& buffer, GLenum const& vertexType, bool const& enableVAA = true) const;
+                        /**
+                         * Generate the buffer's id with different OpenGL command for different buffer
+                         */
+                        void generateID() override;
+                        /**
+                         * Delete the buffer's id with different OpenGL command for different buffer
+                         */
+                        void deleteID() override;
+                        /**
+                         * Bind the current buffer with his custom target and OpenGL command
+                         */
+                        void bind() const override;
+                        /**
+                         * Unbind the current buffer with his custom target and OpenGL command
+                         */
+                        void unbind() const override;
+                        /**
+                         * Access all VBO's buffer to bind the vao with all buffers
+                         * @param buffer     the VBO to access and bind
+                         * @param vertexType the vertex type inside the VBO
+                         * @param enableVAA tell if the function has to call glEnableVertexAttribArray
+                         */
+                        void access(VBO const& buffer, GLenum const& vertexType, bool const& enableVAA = true) const;
 
                     //## Access Operator ##//
 
                     //## Assignment Operator ##//
+                        /**
+                         * No copy assigment allowed
+                         * @param buf the vao to copy
+                         */
+                        VAO& operator=(VAO const& buf) = delete;
+                        /**
+                         * Move assigment of buf into this, leaving buf empty
+                         * @param buf the vao to move into this
+                         * @return the reference of himself
+                         */
+                        VAO& operator=(VAO && buf);
 
                     //## Shortcut Operator ##//
 
@@ -69,11 +119,6 @@
 
                 private:
             };
-
-            inline std::ostream& operator<<(std::ostream &stream, VAO const& arr) {
-                stream << "(" << arr.getID() << ")";
-                return stream;
-            }
 
         };
     };
