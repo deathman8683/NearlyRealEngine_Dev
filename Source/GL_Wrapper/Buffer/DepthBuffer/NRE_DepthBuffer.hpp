a806fc8dd685be6881e18a9a8e055937f487be96
ABEL Louis <abel.louis.88@gmail.com>
2018-04-18 18:04:23 +0200

    Documentation Phase : DepthBuffer

--- Source/GL_Wrapper/Buffer/DepthBuffer/NRE_DepthBuffer.hpp
+++ Source/GL_Wrapper/Buffer/DepthBuffer/NRE_DepthBuffer.hpp
@@ -31,24 +31,44 @@
 
                 public:
                     //## Constructor ##//
-                    DepthBuffer();
+                        /**
+                         * Default Constructor
+                         */
+                        DepthBuffer();
 
                     //## Copy-Constructor ##//
 
                     //## Convertor ##//
 
                     //## Deconstructor ##//
-                    virtual ~DepthBuffer();
+                        /**
+                         * DepthBuffer Deconstructor
+                         */
+                        virtual ~DepthBuffer();
 
                     //## Getter ##//
 
                     //## Setter ##//
 
                     //## Methods ##//
-                    virtual void bind() const = 0;
-                    virtual void unbind() const = 0;
-                    virtual void attach(GLenum const& attachment) const = 0;
-                    void clampToBorder(Maths::Vector4D<NREfloat> const& borderColor) const;
+                        /**
+                         * Bind the current buffer with his custom target and OpenGL command
+                         */
+                        virtual void bind() const = 0;
+                        /**
+                         * Unbind the current buffer with his custom target and OpenGL command
+                         */
+                        virtual void unbind() const = 0;
+                        /**
+                         * Attach the current buffer to a specific attachment point
+                         * @param attachment the frame buffer specific attachment point
+                         */
+                        virtual void attach(GLenum const& attachment) const = 0;
+                        /**
+                         * Apply a specific filter to clamp the depth buffer to a border with a given color
+                         * @param borderColor the border color
+                         */
+                        void clampToBorder(Maths::Vector4D<NREfloat> const& borderColor) const;
 
                     //## Access Operator ##//
 
