40eac289a80c454e0f44403adc9da41ddb902a26
ABEL Louis <abel.louis.88@gmail.com>
2018-05-11 13:00:37 +0200

    Add Shader : PBR

---
+++ Source/Renderer/Shader/EngineShader/PBR/NRE_PBRShader.cpp
@@ -0,0 +1,54 @@
+
+    #include "NRE_PBRShader.hpp"
+
+    namespace NRE {
+        namespace Renderer {
+
+            GLuint PBRShader::MAX_LIGHTS = 10;
+
+            PBRShader::PBRShader() : Shader::Shader(true) {
+                addUniformLocation("texDepth");
+                addUniformLocation("texDiffuse");
+                addUniformLocation("texNormal");
+                addUniformLocation("irradianceMap");
+                addUniformLocation("prefilterMap");
+                addUniformLocation("brdfLUT");
+                for (GLuint i = 0; i < MAX_LIGHTS; i = i + 1) {
+                    std::ostringstream index;
+                    index << i;
+                    addUniformLocation("lights[" + index.str() + "].position");
+                    addUniformLocation("lights[" + index.str() + "].intensities");
+                    addUniformLocation("lights[" + index.str() + "].direction");
+                    addUniformLocation("lights[" + index.str() + "].angle");
+                }
+                for (unsigned int i = 0; i < World::VoxelTypes::getSize(); i = i + 1) {
+                    std::ostringstream index;
+                    index << i;
+                    addUniformLocation("materials[" + index.str() + "].albedo");
+                    addUniformLocation("materials[" + index.str() + "].metallic");
+                    addUniformLocation("materials[" + index.str() + "].roughness");
+                }
+                addUniformLocation("cameraV");
+                addUniformLocation("invModelview");
+                addUniformLocation("invProjection");
+                addUniformLocation("rotation");
+                addUniformLocation("numLights");
+            }
+
+            PBRShader::PBRShader(PBRShader && s) : Shader::Shader(std::move(s)) {
+            }
+
+            PBRShader::~PBRShader() {
+            }
+
+            std::string const PBRShader::getPath() const {
+                return "PBR";
+            }
+
+            PBRShader& PBRShader::operator=(PBRShader && s) {
+                Shader::operator=(std::move(s));
+                return *this;
+            }
+
+        };
+    };
---
+++ Source/Renderer/Shader/EngineShader/PBR/NRE_PBRShader.hpp
@@ -0,0 +1,105 @@
+
+    /**
+     * @file NRE_PBRShader.hpp
+     * @brief Declaration of Engine's Renderer's Object : PBRShader
+     * @author Louis ABEL
+     * @version 1.0
+     */
+
+    #pragma once
+
+    #include <sstream>
+    #include <fstream>
+    #include "../../NRE_Shader.hpp"
+    #include "../../../../World/Voxel/Type/VoxelTypes/NRE_VoxelTypes.hpp"
+
+    /**
+     * @namespace NRE
+     * @brief The global NearlyRealEngine's namespace
+     */
+    namespace NRE {
+        /**
+         * @namespace Renderer
+         * @brief Engine's Renderer Module
+         */
+        namespace Renderer {
+
+            /**
+             * @class PBRShader
+             * @brief Renderer's Object : Manage the pbr specific shader
+             */
+            class PBRShader : public Shader {
+                private:
+
+                public:
+                    //## Constructor ##//
+                        /**
+                         * Default Constructor
+                         */
+                        PBRShader();
+
+                    //## Copy-Constructor ##//
+                        /**
+                         * No copy allowed
+                         * @param s the pbr shader to copy the content
+                         */
+                        PBRShader(PBRShader const& s) = delete;
+
+                    //## Move-Constructor ##//
+                        /**
+                         * Move s into this, leaving s empty
+                         * @param s the pbr shader to move
+                         */
+                        PBRShader(PBRShader && s);
+
+                    //## Convertor ##//
+
+                    //## Deconstructor ##//
+                        /**
+                         * PBRShader Deconstructor
+                         */
+                        ~PBRShader();
+
+                    //## Getter ##//
+                        /**
+                         * Path getter
+                         * @return the shader's specific name path
+                         */
+                        std::string const getPath() const override;
+
+                    //## Setter ##//
+
+                    //## Methods ##//
+
+                    //## Access Operator ##//
+
+                    //## Assignment Operator ##//
+                        /**
+                         * No copy allowed
+                         * @param s the pbr shader to copy into this
+                         * @return the reference of himself
+                         */
+                        PBRShader& operator=(PBRShader const& s) = delete;
+                        /**
+                         * Move assigment of s into this, leaving s empty
+                         * @param s the pbr shader to move into this
+                         * @return the reference of himself
+                         */
+                        PBRShader& operator=(PBRShader && s);
+
+                    //## Shortcut Operator ##//
+
+                    //## Arithmetic Operator ##//
+
+                    //## Comparison Operator ##//
+
+                    //## BitWise Operator ##//
+
+                    //## Shift Operator ##//
+
+                private:
+                    static GLuint MAX_LIGHTS;
+            };
+
+        };
+    };
